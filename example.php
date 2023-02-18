ba8ebb21e8ccbda69eaa902e4083fd84ead6d57d
saratahani <123387435+saratahani@users.noreply.github.com>
2023-02-18 05:03:05 +0330

    Update example.php

--- example.php
+++ example.php
@@ -9,12 +9,12 @@ $proxy1 = $tpc->getNewProxy(); //Get last proxy in channel
 
 $proxy2 = $tpc->getNewProxy(); //Get a proxy before the last
 
-// and etc ...
+
 
 
 $count = $tpc->getCount(); // Get count of all proxies that collector can collect
 
-$count = $tpc->getAll(); // Get all proxies that collector can collect
+# $count = $tpc->getAll(); // Get all proxies that collector can collect
 
 
 
